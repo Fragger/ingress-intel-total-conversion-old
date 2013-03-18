adb88ae4b1b246686f6a715bbc16b46c22e02bf2
Kevin <wonderfulweb@gmail.com>
2013-03-17 18:39:46 -0700

    Update to build plugins, move to built folder

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ import time
 import re
 import io
 import base64
+import os
 
 def readfile(fn):
     with io.open(fn, 'Ur', encoding='utf8') as f:
@@ -22,21 +23,37 @@ def loaderImage(var):
     fn = var.group(1)
     return 'data:image/png;base64,{0}'.format(str(base64.encodestring(open(fn, 'rb').read())).replace('\n', ''))
 
+def subIncludes(m):
+    m = m.replace('@@BUILDDATE@@', time.strftime('%Y-%m-%d-%H%M%S'))
+    m = re.sub('@@INCLUDERAW:([0-9a-zA-Z_./-]+)@@', loaderRaw, m)
+    m = re.sub('@@INCLUDESTRING:([0-9a-zA-Z_./-]+)@@', loaderString, m)
+    m = re.sub('@@INCLUDEIMAGE:([0-9a-zA-Z_./-]+)@@', loaderImage, m)
+    return m
 
 c = '\n\n'.join(map(readfile, glob.glob('code/*')))
-n = time.strftime('%Y-%m-%d-%H%M%S')
 m = readfile('main.js')
 
 m = m.split('@@INJECTHERE@@')
 m.insert(1, c)
 m = '\n\n'.join(m)
 
-m = m.replace('@@BUILDDATE@@', n)
-m = re.sub('@@INCLUDERAW:([0-9a-zA-Z_./-]+)@@', loaderRaw, m)
-m = re.sub('@@INCLUDESTRING:([0-9a-zA-Z_./-]+)@@', loaderString, m)
-m = re.sub('@@INCLUDEIMAGE:([0-9a-zA-Z_./-]+)@@', loaderImage, m)
+m = subIncludes(m)
 
-with io.open('iitc-debug.user.js', 'w', encoding='utf8') as f:
+try:
+    os.makedirs('built/plugins')
+except OSError as exception:
+    if exception.errno != errno.EEXIST:
+        raise
+
+with io.open('built/iitc-debug.user.js', 'w', encoding='utf8') as f:
     f.write(m)
+
+for fn in glob.glob('plugins/*.js'):
+  m = readfile(fn)
+
+  m = subIncludes(m)
+
+  with io.open('built/' + fn, 'w', encoding='utf8') as f:
+      f.write(m)
 
 # vim: ai si ts=4 sw=4 sts=4 et
